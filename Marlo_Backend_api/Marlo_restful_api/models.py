1b885f0662c70736b24ab984f937052d0347f221
shivamkumarsingh28 <110661272+shivamkumarsingh28@users.noreply.github.com>
2023-08-22 16:30:58 +0530

    Update models.py

--- Marlo_Backend_api/Marlo_restful_api/models.py
+++ Marlo_Backend_api/Marlo_restful_api/models.py
@@ -8,3 +8,15 @@ class MarloUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class MarloProduct(models.Model):
+    name = models.CharField(max_length=50)
+    barcode = models.IntegerField()
+    brand = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.IntegerField()
+    available = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
