f4755803a2e20174cedf6040f92ca2acaf3ccb59
shivamkumarsingh28 <110661272+shivamkumarsingh28@users.noreply.github.com>
2023-08-22 16:30:08 +0530

    Update urls.py

--- Marlo_Backend_api/Marlo_restful_api/urls.py
+++ Marlo_Backend_api/Marlo_restful_api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import Marlo_registerUser, Marlo_user_login, Marlo_user_logout
+from .views import Marlo_registerUser, Marlo_user_login, Marlo_user_logout,  MarloProductListView, ApiOverview
 
 urlpatterns = [
+    path('', ApiOverview, name='home'),
     path('register/', Marlo_registerUser, name='register'),
+    path('product/', MarloProductListView.as_view(), name='snippet-list'),
     path('login/', Marlo_user_login, name='login'),
     path('logout/', Marlo_user_logout, name='logout'),
 ]
